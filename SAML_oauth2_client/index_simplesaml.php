b8b8d9b8f0a6accfb21610574fe9956ce9c908de
wvengen <wvengen@nikhef.nl>
2011-11-03 16:04:43 +0100

    allow to select SimpleSAMLphp authsource in saml client demo

--- SAML_oauth2_client/index_simplesaml.php
+++ SAML_oauth2_client/index_simplesaml.php
@@ -7,47 +7,74 @@ include_once('../oauth_client/src/OAuth.class.php');
 require_once('utils/simplesamlphp/lib/_autoload.php');
 
 $content='';
-//We select our authentication source:
-$sp = 'default-sp-userpass';
-$as = new SimpleSAML_Auth_Simple($sp);
+
+function error($msg) {
+  $content.="<div class='error'>SimpleSAML error: ".$e->getMessage()."</div>";
+  include 'html/template.php';
+  exit(1);
+}
+
+// authsource must be selected before we can use SimpleSAMLphp
+$sp = null;
+$as = null;
+if (isset($_REQUEST['authsource'])) {
+    $sp = $_REQUEST['authsource'];
+    try {
+        $as = new SimpleSAML_Auth_Simple($sp);
+    } catch(Exception $e) {
+        error("SimpleSAML error: ".$e->getMessage());
+    }
+}
 
 // handle logout
-if (isset($_REQUEST['logout'])) {
-	if ($as->isAuthenticated()) {
-		try {
-			$as->logout();
-		} catch(Exception $e) {
-			$content.="<div class='error'>SimpleSAML error: ".$e->getMessage()."</div>";
-		}
-	} else {
-		$content .= "<p>You been logged out.</p>";
-		$content .= "<p><a href='?login'>Play again</a>.</p>";
-	}
+if (isset($_REQUEST['logout']) && !empty($as)) {
+    try {
+        if ($as->isAuthenticated()) $as->logout();
+    } catch(Exception $e) {
+        error("SimpleSAML error: ".$e->getMessage());
+    }
+}
 
-// handle normal flow
+// authsource selection on first entry or logout
+if (!isset($_REQUEST['authsource']) || isset($_REQUEST['logout'])) {
+    // first visit or just logged out: select authsource
+    $authcfg = SimpleSAML_Configuration::getConfig('authsources.php');
+    $authsources = $authcfg->getOptions();
+    $authsdfl = @$_REQUEST['authsource'];
+    $content .= "<p>You are not logged in.</p>\n";
+    $content .= "<form action='".$_SERVER['REQUEST_URI']."' method='get'>\n";
+    $content .= "  <input type='hidden' name='login' value='1'/>\n";
+    $content .= "  Login using <select name='authsource'>\n";
+    foreach ($authsources as $as) {
+      if (empty($authsdfl) && strncmp($as,"default",7)==0) $authsdfl = $as;
+      $opt = ($authsdfl==$as) ? " selected='selected'" : "";
+      $content .= "    <option$opt>$as</option>\n";
+    }
+    $content .= "  </select>\n";
+    $content .= "  <input type='submit' name='submit' value='Go'/>\n";
+    $content .= "</form>\n";
+    
 } else {
-
-	
-	//We then require authentication:
-	$as->requireAuth();
-	//And print the attributes:
-	$attributes = $as->getAttributes();
-	error_log(print_r($attributes,true));
-	$assertion = $attributes;
-	if($assertion=="") {
-		$content.="<div class='error'>Access denied.</div>";
-	} else {
-		$sho=@$assertion['urn:mace:dir:attribute-def:eduPersonScopedAffiliation'][0];
-		if (is_null($sho)) $sho = @$assertion['eduPersonScopedAffiliation'][0];
-		$content .="<p>Because of your affiliation <b>'".$sho."'</b>, you can access the following services:";
-		$client = new OAuth(dirname(__FILE__)."/own_config");
-		if(!$client->doOAuthFlow($assertion)){
-		    $content.=$client->getError();
-		}else{
-		    $content.=$client->getResource();
-		}        
-		$content .= "<p><a href='?logout'>logout</a></p>";
-	}
+    // authsource selected: login and show resource
+    $as->requireAuth();
+    //And print the attributes:
+    $attributes = $as->getAttributes();
+    error_log(print_r($attributes,true));
+    $assertion = $attributes;
+    if($assertion=="") {
+    	$content.="<div class='error'>Access denied.</div>";
+    } else {
+    	$client = new OAuth(dirname(__FILE__)."/own_config");
+    	if(!$client->doOAuthFlow($assertion)){
+    	    $content.="<div class='error'>".$client->getError()."</div>";
+    	}else{
+    	    $content.="<p>The resource server returned the following message:</p>";
+    	    $content.="<p><i>";
+    	    $content.=$client->getResource();
+    	    $content.="</i></p>";
+    	}        
+    	$content .= "<p><a href='?logout&amp;authsource=$sp'>logout</a></p>";
+    }
 }
 include 'html/template.php';
 ?>  
